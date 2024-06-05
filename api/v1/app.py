04cc98c21fa715cf01bcd70ddb6ec4ea7ef000c0
BeygonK <kipkoechbeygone@gmail.com>
2024-06-05 12:59:44 +0300

    error handelr

--- api/v1/app.py
+++ api/v1/app.py
@@ -9,13 +9,16 @@ import os
 app = Flask(__name__)
 app.register_blueprint(app_views, url_prefix='/api/v1')
 
+
 @app.teardown_appcontext
 def teardown(exception):
     storage.close()
 
+
 @app.errorhandler(404)
 def not_found(error):
     return jsonify({"error": "Not found"}), 404
+
 
 if __name__ == "__main__":
     host = os.getenv('HBNB_API_HOST', '0.0.0.0')
