f744b1babe94e45526ccd8338962184d6ba0ec97
BeygonK <kipkoechbeygone@gmail.com>
2024-06-05 12:51:50 +0300

    error handelr

--- api/v1/app.py
+++ api/v1/app.py
@@ -10,6 +10,10 @@ app.register_blueprint(app_views, url_prefix='/api/v1')
 def teardown(exception):
     storage.close()
 
+@app.errorhandler(404)
+def not_found(error):
+    return jsonify({"error": "Not found"}), 404
+
 if __name__ == "__main__":
     host = os.getenv('HBNB_API_HOST', '0.0.0.0')
     port = int(os.getenv('HBNB_API_PORT', 5000))
